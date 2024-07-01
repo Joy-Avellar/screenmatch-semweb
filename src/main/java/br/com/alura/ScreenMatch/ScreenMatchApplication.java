da5eeead4aa680c9a50449ee27b9af4be0302622
Joy <joy.avellar@gmail.com>
2024-06-30 21:17:40 -0300

    first commit

---
+++ src/main/java/br/com/alura/ScreenMatch/ScreenMatchApplication.java
@@ -0,0 +1,31 @@
+package br.com.alura.ScreenMatch;
+
+import br.com.alura.ScreenMatch.Model.DadosSerie;
+import br.com.alura.ScreenMatch.service.ConsumoApi;
+import br.com.alura.ScreenMatch.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenMatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenMatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("http://www.omdbapi.com/?t=grey's+anatomy&apikey=7d277e04");
+		System.out.println(json);
+
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+
+
+
+
+	}
+}
